51e6ba08ab85f4652c78fbf4f1e268124b98d43a
Bercin <bercintatar@gmail.com>
2023-07-20 01:25:20 +0300

    Closures

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = header.style.color === 'red' ? 'blue' : 'red';
+  });
+}
+changeColor();
